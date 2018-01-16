dbec341638a9492239df6ed7c33faf07eeb335af
scott.cgi <scott.cgi@qq.com>
2018-01-16 17:20:13 +0800

    Update java copyright

--- Samples/SuperLittleRed/Android/app/src/main/java/com/Mojoc/Samples/SuperLittleRed/Activity.java
+++ Samples/SuperLittleRed/Android/app/src/main/java/com/Mojoc/Samples/SuperLittleRed/Activity.java
@@ -6,14 +6,12 @@ import android.view.KeyEvent;
 import android.view.View;
 
 /**
- * Copyright (c) 2012-2017 scott.cgi All Rights Reserved.
+ * Copyright (c) 2012-2018 scott.cgi All Rights Reserved.
  *
  * This code is licensed under the MIT License.
  *
- * Since  : 2015-03-02
- * Author : scott.cgi
- * Version: 0.0.0
- *
+ * Since : 2015-03-02
+ * Author: scott.cgi
  */
 public class Activity extends NativeActivity {
 
