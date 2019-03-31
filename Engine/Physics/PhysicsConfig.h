f38a8c1dbdbb66cf2b2954d91185edc657e0b858
scottcgi <scott.cgi@qq.com>
2019-03-31 15:19:34 +0800

    Update Copyright info.

--- Engine/Physics/PhysicsConfig.h
+++ Engine/Physics/PhysicsConfig.h
@@ -1,10 +1,17 @@
 /*
  * Copyright (c) 2012-2019 scott.cgi All Rights Reserved.
  *
- * This code and its project Mojoc are licensed under [the MIT License],
- * and the project Mojoc is a game engine hosted on github at [https://github.com/scottcgi/Mojoc],
- * and the author's personal website is [https://scottcgi.github.io],
- * and the author's email is [scott.cgi@qq.com].
+ * This source code is licensed under the MIT License,
+ * and the license can be found at: https://github.com/scottcgi/Mojoc/blob/master/LICENSE
+ *
+ * This source code belongs to project Mojoc which is a pure C game engine hosted on GitHub,
+ * and the details can be found at: https://github.com/scottcgi/Mojoc
+ *
+ * The author information can be found at:
+ * https://github.com/scottcgi
+ *
+ * The project Mojoc is a serious project with extreme code pursuit,
+ * and will continue to iterate.
  *
  * Since : 2014-8-7
  * Author: scott.cgi
