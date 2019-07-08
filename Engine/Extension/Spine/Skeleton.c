89e3d79dcefc325944f63eb5a5b9815f3a138e5f
scottcgi <scott.cgi@qq.com>
2019-07-08 10:46:23 +0800

    Update copyright.

--- Engine/Extension/Spine/Skeleton.c
+++ Engine/Extension/Spine/Skeleton.c
@@ -1,9 +1,11 @@
 /*
  * Copyright (c) 2012-2019 scott.cgi All Rights Reserved.
  *
- * This source code is licensed under the MIT License.
- * This source code belongs to project Mojoc which is a pure C game engine hosted on GitHub.
- * This Mojoc game engine is a serious project with extreme code pursuit, and will continue to iterate.
+ * This source code belongs to project Mojoc, which is a pure C Game Engine hosted on GitHub.
+ *
+ * The Mojoc Game Engine is a serious project with extreme code pursuit,
+ * and will continue to iterate,
+ * and licensed under the MIT License.
  *
  * License: https://github.com/scottcgi/Mojoc/blob/master/LICENSE
  * GitHub : https://github.com/scottcgi/Mojocate.
