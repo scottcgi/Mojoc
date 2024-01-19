bb69b9465c18b17b261ea5fab8b972aa8ed0fb45
scottcgi <scott.cgi@qq.com>
2024-01-19 14:20:20 +0800

    Adjust code comments.

--- Engine/Graphics/Draw/Drawable.c
+++ Engine/Graphics/Draw/Drawable.c
@@ -2,10 +2,7 @@
  * Copyright (c) scott.cgi All Rights Reserved.
  *
  * This source code belongs to project Mojoc, which is a pure C Game Engine hosted on GitHub.
- *
- * The Mojoc is a serious project with extreme code pursuit,
- * and will continue to iterate,
- * and licensed under the MIT License.
+ * The Mojoc Game Engine is licensed under the MIT License, and will continue to be iterated with coding passion.
  *
  * License  : https://github.com/scottcgi/Mojoc/blob/master/LICENSE
  * GitHub   : https://github.com/scottcgi/Mojoc
